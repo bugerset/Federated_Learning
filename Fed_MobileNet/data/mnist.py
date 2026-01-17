67cc76ad382424d3e4ef38c7523c94d13b038179
Youngseo Lee <107788991+bugerset@users.noreply.github.com>
2026-01-17 18:15:09 +0900

    Add files via upload

---
+++ Fed_MobileNet/data/mnist.py
@@ -0,0 +1,39 @@
+from torchvision import datasets, transforms
+
+def get_mnist(root="./data", normalize = True, augment = False):
+
+    # Normalize MNIST with below values
+    mean = (0.1307)
+    std  = (0.3081)
+
+    train_tf = []
+
+    if augment:
+        # RandomCrop & HorizontalFlip
+        train_tf += [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+        ]
+
+    train_tf.append(transforms.ToTensor())
+    test_tf = [transforms.ToTensor()]
+
+    if normalize:
+        train_tf.append(transforms.Normalize(mean, std))
+        test_tf.append(transforms.Normalize(mean, std))
+
+    train_data = datasets.MNIST(
+        root=root,   
+        train=True, 
+        download=True,
+        transform=transforms.Compose(train_tf)
+    )
+
+    test_data  = datasets.MNIST(
+        root=root, 
+        train=False, 
+        download=True,
+        transform=transforms.Compose(test_tf)
+    )
+    
+    return train_data, test_data
