774bf53c013885d5007c0ae6c769cdd98fea17e9
Youngseo Lee <107788991+bugerset@users.noreply.github.com>
2026-01-25 06:03:11 +0900

    Update mnist.py

--- Fed_MobileNet/data/mnist.py
+++ Fed_MobileNet/data/mnist.py
@@ -12,7 +12,6 @@ def get_mnist(root="./data", normalize = True, augment = False):
         # RandomCrop & HorizontalFlip
         train_tf += [
             transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
         ]
 
     train_tf.append(transforms.ToTensor())
